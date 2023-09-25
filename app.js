24072caa02f62ac367781b32a204fa7f80c56a85
anup arun <anuparun81@gmail.com>
2023-09-25 17:11:58 +0530

    second commit

--- app.js
+++ app.js
@@ -22,15 +22,19 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// coonect the mongodb database
 mongoose.connect("mongodb+srv://anuparun81:mongodB@cluster0.8gy6271.mongodb.net/todolistdb", { useNewUrlParser: true });
 
+// created a name schema 
 const itemsSchenma = {
     name: String
 
 }
 
+// createing a model name Item
 const Item = mongoose.model("Item", itemsSchenma);
 
+// create three default Item name 
 const item1 = new Item({
     name: "Welcome to your todolist "
 })
@@ -41,22 +45,26 @@ const item3 = new Item({
     name: "<-- hit this to delete item "
 })
 
+// push all three intem 
 const defaultItems = [item1, item2, item3];
 
+//defining the schema for some kind of list or collection
 const listSchema = {
     name: String,
     items: [itemsSchenma]
 };
 
-
+// createing a model name List
 const List = mongoose.model("List", listSchema);
 
 app.use(express.static("public"));
 
+//crete  home route of get request
 app.get("/", function (req, res) {
 
     let today = new Date();
     //  var currentday =today.getDate();
+    // check if already listtitle is prsent or not
     Item.find({}).then(function (foundItems) {
         if (foundItems.length === 0) {
             Item.insertMany(defaultItems).then(function () {
@@ -86,6 +94,7 @@ app.get("/", function (req, res) {
 
 });
 
+ //post route of get request (add item in list)
 app.post("/", function (req, res) {
 
     let itemName = req.body.newItem;
@@ -112,6 +121,7 @@ app.post("/", function (req, res) {
     }
 });
 
+// delete route for deleteinng item from list
 app.post("/delete", function (req, res) {
     const checkedItemId = req.body.checkbox;
      const listName = req.body.listName;
@@ -145,7 +155,7 @@ app.post("/delete", function (req, res) {
       
 });
 
-
+// route for customlist name
 app.get("/:customListName", function (req, res) {
      const customListName =_.capitalize(req.params.customListName);
     // const customListName = (req.params.customListName);
@@ -184,7 +194,7 @@ app.get("/about", function (req, res) {
     res.render("about");
 })
 
-
+// route for local host connection
 app.listen(3000, function () {
     console.log("server is running on port 3000 ");
 })
